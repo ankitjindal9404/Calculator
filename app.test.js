ef58cff4c7011a561d64ee9508f55a939c1af10b
Ankit Jindal <jindalankit940@gmail.com>
2025-07-18 19:56:02 +0530

    Added code

---
+++ app.test.js
@@ -0,0 +1,30 @@
+// calculator.test.js
+const Calculator = require('./calculator');
+
+describe('Calculator Tests', () => {
+    let calc;
+
+    beforeEach(() => {
+        calc = new Calculator();
+    });
+
+    test('add: 1 + 2 should equal 3', () => {
+        expect(calc.add(1, 2)).toBe(3);
+    });
+
+    test('subtract: 5 - 3 should equal 2', () => {
+        expect(calc.subtract(5, 3)).toBe(2);
+    });
+
+    test('multiply: 2 * 3 should equal 6', () => {
+        expect(calc.multiply(2, 3)).toBe(6);
+    });
+
+    test('divide: 6 / 2 should equal 3', () => {
+        expect(calc.divide(6, 2)).toBe(3);
+    });
+
+    test('divide: should throw error when dividing by 0', () => {
+        expect(() => calc.divide(5, 0)).toThrow("Cannot divide by zero");
+    });
+});
